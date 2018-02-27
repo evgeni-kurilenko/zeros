658fd71da355becd3426674d6f326effc16f1bcb
Evgeni Kurilenko <evgeni.kurilenko@gmail.com>
2018-02-27 17:09:41 +0000

    Task6 solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+    let zeros = 0;
+    let j = 1;
+    if (number>4) {
+        for (let i=5; i<=number; i+=5) {
+            j = i;
+            while (j%5==0) {
+                j/=5;
+                zeros++;
+            }
+        }
+    }
+    return zeros;
 }
